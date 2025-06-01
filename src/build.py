dec666fa544f3621cbe2c52fc287274f00a98f4c
Hamilchin <chinalexan@gmail.com>
2025-06-01 00:45:58 -0700

    make better build software

--- src/build.py
+++ src/build.py
@@ -28,8 +28,8 @@ def find_paths_from_filenames(filenames, current_dir, path_dict):
     return path_dict
 
 
-def make_html_links(paths):
-    return ".".join([f'<a href="{path}">{".".join(os.path.basename(path).split(".")[:-1])}</a> </br>' for path in paths])
+def make_html_links(posts): #posts being posts from post_structure
+    return "".join([f'<a href="{posts[title]["page_path"]}">{posts[title]["display"]}</a> </br>' for title in posts])
 
 
 def make_html_from_template(template_string, **kwargs):
@@ -47,49 +47,63 @@ def parse_file_structure(filename):
     file_structure = {}
     for line in lines:
         if line[-1] == ":":
-            file_structure[line[:-1]] = []
+            file_structure[line[:-1]] = {}
             current_category = line[:-1]
         else:
-            file_structure[current_category].append([x.strip() for x in line.split("|")])
-            # (display, file_name)
-    
+            post = {}
+            data = [x.strip() for x in line.split("|")]
+            post["display"], post["file_name"] = data[1].lower(), data[2]
+            post["local_path"], post["page_path"]= None, None
+            title = data[0]
+            file_structure[current_category][title] = post
     return file_structure
 
 
 
 def main():
-    file_structure = parse_file_structure("files.txt")
+    post_structure = parse_file_structure("files.txt")
 
-    print(json.dumps(file_structure, indent=2))
+    #data completion: find local paths
+    for category in post_structure:
+        posts = post_structure[category]
+        filenames = [posts[title]["file_name"] for title in posts]
+        path_dict = find_paths_from_filenames(filenames, vault_dir, {})
+        for title in posts:
+            filename = posts[title]["file_name"]
+            posts[title]["local_path"] = path_dict[filename] if filename in path_dict else print(f"error: {filename} not found in vault")
 
-    category_page_paths = {}
 
-    for category in file_structure:
+    print(json.dumps(post_structure, indent=2))
 
-        if category not in ["sketches"]: #auto-constructing md
+    #writing post html files
+    for category in post_structure:
+        if category not in ["sketches"]: #auto-constructing categories (need to make html)
+
             os.makedirs(category, exist_ok=True)
 
-            
-                template = open(os.path.join("src","page_template.html"), "r").read()
-                md_content = open(path, "r").read()
+            for title in post_structure[category]:
 
-                html = make_html_from_template(template, title=doc_name, content=md.markdown(md_content, extensions=extensions))
-
-                page_path = os.path.join(category, doc_name + ".html")
-                page_paths.append(page_path)
-
+                page_template = open(os.path.join("src","page_template.html"), "r").read()
+                local_path = post_structure[category][title]["local_path"]
+                md_content = open(local_path, "r").read()
+                html = make_html_from_template(page_template, title=title, content=md.markdown(md_content, extensions=extensions))
+                page_path = os.path.join(category, title + ".html")
+                post_structure[category][title]["page_path"] = page_path
                 with open(page_path, "w") as f:
                     f.write(html)
 
-        else: #manual control; every file in files.txt must map to a real file
-             
-             category_page_paths[category] = file_structure[category]
+        else: #manual categories (assume already have files)
+            for title in post_structure[category]:
+                filename = post_structure[category][title]["file_name"]
+                post_structure[category][title]["page_path"] = os.path.join(category, filename)
 
 
     template = open(os.path.join("src", "index_template.html"), "r").read()
-    index_html = make_html_from_template(template, poem_links=make_html_links(category_page_paths["poems"]), 
-                                        sketches_links=make_html_links(category_page_paths["sketches"]),
-                                        idea_garden_links=make_html_links(category_page_paths["idea-garden"]))
+
+
+    index_html = make_html_from_template(template, poem_links=make_html_links(post_structure["poems"]), 
+                                        sketches_links=make_html_links(post_structure["sketches"]),
+                                        idea_garden_links=make_html_links(post_structure["idea-garden"]))
     with open("index.html", "w") as f:
                 f.write(index_html)
 
